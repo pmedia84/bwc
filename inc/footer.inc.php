0a6e77b889fb2956823e50894bf1a983cacbcc99
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-01-24 09:34:55 +0000

    footer

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -9,7 +9,7 @@
             <ul>
                 <li><a href="index">Home</a></li>
                 <li><a href="about">About Us</a></li>
-                <li><a href="services">Our Services</a></li>
+                <li><a href="window-cleaning-services-cambridgeshire">Our Services</a></li>
                 <li><a href="contact">Contact Us</a></li>
                 <li><a href="privacy">Privacy Policy</a></li>
              
